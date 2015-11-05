05a7a3470989819a8c63afb73285150e624289f1
Fabio Gouw <fabiogouw@gmail.com>
2015-11-05 20:23:09 -0200

    Lesson 3 of week 4 ok

---
+++ Box Shooter/Assets/Scripts/BasicController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Horizontal input = " + Input.GetAxis("Horizontal"));
+	}
+}
