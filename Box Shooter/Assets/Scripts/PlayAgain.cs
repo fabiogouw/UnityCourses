7d40d4837415b3f49bf22a3347da858949407f2d
Fabio Gouw <fabiogouw@gmail.com>
2015-11-05 22:49:28 -0200

    Lesson 4 of week 4 ok;

--- Box Shooter/Assets/Scripts/PlayAgain.cs
+++ Box Shooter/Assets/Scripts/PlayAgain.cs
@@ -8,7 +8,7 @@ public class PlayAgain : MonoBehaviour {
 	{
 		// only do stuff if hit by a projectile
 		if (newCollision.gameObject.tag == "Projectile") {
-			// call the RestartGame function in the game manager
+            // call the RestartGame function in the game manager
 			GameManager.gm.RestartGame();
 		}
 	}
