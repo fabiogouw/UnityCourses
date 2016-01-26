68453019cffec1101edec48584458945858d6d2e
Fabio Gouw <fabiogouw@gmail.com>
2016-01-26 20:14:56 -0200

    Final assignment: animating the vanishing platform

--- SuperSpartyBros/Assets/Scripts/PlatformVanisher.cs
+++ SuperSpartyBros/Assets/Scripts/PlatformVanisher.cs
@@ -6,15 +6,19 @@ public class PlatformVanisher : MonoBehaviour {
 
 	public GameObject platform; // reference to the platform to vanish
 
-    [Range(3, 7)]
+    [Range(5, 9)]
     [Tooltip("How long the platform will remain solid or not.")]
-	public float stateTime = 3f; // how long each state will be active
+	public float stateTime = 5f; // how long each state will be active
+    [Range(0, 3)]
+    public int timeDelay = 0;
 
     private bool _visible = true;
+    private bool _vanishing = false;
     private SpriteRenderer _renderer;
     private BoxCollider2D _collider;
     private float _normalAlpha = 1f;
     private float _vanishedAlpha = 0.25f;
+    private Animator _animator;
 
     // private variables
 
@@ -22,17 +26,28 @@ public class PlatformVanisher : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        _renderer = platform.GetComponent<SpriteRenderer>();
-        _normalAlpha = _renderer.material.color.a;
-        _vanishedAlpha = _normalAlpha * 0.25f;
         _collider = platform.GetComponent<BoxCollider2D>();
-        _lastChangeStateTime = 0f;
+        _animator = platform.GetComponent<Animator>();
+        if (_animator == null)
+        {
+            Debug.LogError("Animator component missing from this gameobject");
+        }
+        _lastChangeStateTime = 0f + timeDelay;
 	}
 	
 	// game loop
 	void Update () {
         var currentTime = Time.time;
-        if (currentTime >= (_lastChangeStateTime + stateTime)) {
+        if (currentTime >= (_lastChangeStateTime + stateTime - 1.5f))
+        {
+            if (!_vanishing)
+            {
+                _animator.SetBool("Vanishing", true);
+                _vanishing = true;
+            }
+        }
+        if (currentTime >= (_lastChangeStateTime + stateTime))
+        {
             ChangeState();
             _lastChangeStateTime = currentTime;
         }
@@ -41,8 +56,9 @@ public class PlatformVanisher : MonoBehaviour {
     private void ChangeState()
     {
         _visible = !_visible;
-        float newAlpha = _visible ? _normalAlpha : _vanishedAlpha;
-        _renderer.material.color = new Color(_renderer.material.color.r, _renderer.material.color.g, _renderer.material.color.b, newAlpha);
         _collider.enabled = _visible;
+        _animator.SetBool("Vanishing", false);
+        _vanishing = false;
+        _animator.SetBool("Solid", _visible);
     }
 }
